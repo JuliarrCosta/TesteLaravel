9b670cf17c233b55a840f5141583dd348989c40a
JuliarrCosta <Juliaraquelrc@outlook.com>
2024-06-18 19:16:34 -0300

    Ajustes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/products', [ProductController::class, 'store']);
+Route::put('/products/{id}', [ProductController::class, 'update']);
+Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+Route::post('/register',[AuthController::class, 'register']);
--- routes/web.php
+++ routes/web.php
@@ -7,16 +7,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('teste', function(){
-    return 'Rodou essa desgrama';
-});
-
 //apenas usuários autorizados, ainda falta fazer a autenticação
-    Route::post('/products', [ProductController::class, 'store']);
-    Route::put('/products/{id}', [ProductController::class, 'update']);
-    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+    //Route::post('/products', [ProductController::class, 'store']);
+    //Route::put('/products/{id}', [ProductController::class, 'update']);
+    //Route::delete('/products/{id}', [ProductController::class, 'destroy']);
 
 
 // todos usuários podem acessar essas requests
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
+
+Route::get('/login', function(){
+    return view('login');
+});
